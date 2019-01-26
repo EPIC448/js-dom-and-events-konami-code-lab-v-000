a1d90877d5c94ce5c1771de95c69c0a40a3e7d11
epic0448 <samuelogundiran@Yahoo.com>
2019-01-26 09:47:02 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,15 +2,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
 
-function init(e) {
+function init() {
   let index = 0
-  document.body.addEventListener('keydown', function(){
+  document.body.addEventListener('keydown', function(e){
 
   const key = parseInt(e.detail || e.which);
 
-  if (key === alphabet[index]){ index++;
+  if (key === code[index]){ index++;
 
-  if (index === alphabet.length){
+  if (index === code.length){
         alert("alert")
         index = 0
     }
